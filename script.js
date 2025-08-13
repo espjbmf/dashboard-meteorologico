5aed976f0b63fa7aa5ec2c85dcce44a222dbdbc5
espjbmf <espanemometro@gmail.com>
2025-08-13 14:48:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -142,7 +142,7 @@ function analisarCondicoes(temperatura, umidade, vento, pontoOrvalho) {
             : { texto: "Tempo Quente", icone: ICONE_SOL };
     } else if (temperatura > 20) {
         return (umidade < 40)
-            ? { texto: "Clima Ameno e Seco", icone: ICONE_SOL }
+            ? { texto: "Ameno e Seco", icone: ICONE_SOL }
             : { texto: "Tempo Agradável", icone: ICONE_SOL };
     } else if (temperatura > 13) {
         return (umidade > 80)
@@ -171,4 +171,5 @@ function classificarPotencialEolico(ventoKmh) {
         return { texto: 'Calmo', cor: '#8a8d93' };
     }
 }
+
 
